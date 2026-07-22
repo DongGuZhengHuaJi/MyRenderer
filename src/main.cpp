aa9d7ad9377136343c22f0bd00bac71aa2b80263
DongGu <2756284698@qq.com>
2026-07-22 14:10:35 +0800

    重构主窗口，添加 MainWindow 类及其 UI 文件，更新主程序以使用新窗口类

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include <QApplication>
 #include <QMainWindow>
 #include "window/RenderViewport.h"
+#include "ui/MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QMainWindow window;
-    window.setWindowTitle("MyRenderer Sandbox - v1.0");
-    window.resize(1024, 768);
-
-    // 将我们的渲染视口作为中央控件嵌入窗口
-    RenderViewport *viewport = new RenderViewport(&window);
-    window.setCentralWidget(viewport);
+    MainWindow window;
+    window.setWindowTitle("MyRenderer");
+    window.resize(1920, 1200); // 设置窗口大小
 
     window.show();
     return a.exec();
